ffb802d48e9e2a1e1caf7578bac648633ddd52d4
daniel ziorli@ <kannomonster@gmail.com>
2021-09-15 14:25:15 -0400

    edit config

--- svelte.config.js
+++ svelte.config.js
@@ -3,10 +3,6 @@ import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
-	preprocess: sveltePreprocess(),
-	basepath: '/portfolio/',
 	kit: {
 		target: '#svelte',
 		adapter: adapter({
