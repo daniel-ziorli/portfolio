66751c862cd22dcdf67b84e99856f80cb818a8f9
Daniel Ziorli <daniel.ziorli@gmail.com>
2024-08-21 02:20:13 -0400

    chore: type

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,5 +1,3 @@
-
-
 <script lang="ts">
   import Navbar from '../components/NavBar.svelte';
   import Footer from '../components/Footer.svelte';
@@ -14,16 +12,22 @@
 
 <svelte:head>
   <title>Daniel Ziorli - Software Engineer</title>
-  <meta name="description" content="Hi I'm Daniel Ziorli; Full stack developer, software engineer and game developer. I create clean and scalable code and absolutely love it." />
-  <meta name="keywords" content="Daniel, Ziorli, Daniel Ziorli, programmer, software engineer, software developer, coder" />
+  <meta
+    name="description"
+    content="Hi I'm Daniel Ziorli; Full stack developer, software engineer and game developer. I have a passion for creating new and innovative things with code!"
+  />
+  <meta
+    name="keywords"
+    content="Daniel, Ziorli, Daniel Ziorli, programmer, software engineer, software developer, full stack developer, game developer, portfolio, projects, game development, unity, c#, python, javascript, html, css, svelte, sapper, vue, angular, nodejs, dart"
+  />
   <meta name="author" content="Daniel Ziorli" />
-  <link rel="apple-touch-icon" sizes="180x180" href="favicon/apple-touch-icon.png">
-  <link rel="icon" type="image/png" sizes="32x32" href="favicon/favicon-32x32.png">
-  <link rel="icon" type="image/png" sizes="16x16" href="favicon/favicon-16x16.png">
-  <link rel="manifest" href="favicon/site.webmanifest">
+  <link rel="apple-touch-icon" sizes="180x180" href="favicon/apple-touch-icon.png" />
+  <link rel="icon" type="image/png" sizes="32x32" href="favicon/favicon-32x32.png" />
+  <link rel="icon" type="image/png" sizes="16x16" href="favicon/favicon-16x16.png" />
+  <link rel="manifest" href="favicon/site.webmanifest" />
 </svelte:head>
 
-<Navbar  />
+<Navbar />
 <slot />
 <Footer />
 
